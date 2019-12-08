4a267cfb0842a76b58e343ba9d3f774c7846df00
shinyay <shinya.com@gmail.com>
2019-12-08 19:47:48 +0900

    Add data class for Person

---
+++ src/main/kotlin/io/pivotal/shinyay/entity/Person.kt
@@ -0,0 +1,7 @@
+package io.pivotal.shinyay.entity
+
+data class Person(val id: Int,
+                  val firstName: String,
+                  val lastName: String,
+                  val age: Int) {
+}
