1897625655a93c51701e12894fa8714239215965
shinyay <shinya.com@gmail.com>
2019-12-09 23:44:21 +0900

    Add reactive stream with delay sequence

--- src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
+++ src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
@@ -3,9 +3,11 @@ package io.pivotal.shinyay.controller
 import io.pivotal.shinyay.entity.Person
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import java.time.Duration
 import java.util.stream.Stream
 
 @RestController
@@ -21,4 +23,15 @@ class PersonController {
                     Person(4, "Name04", "Surname04", 44),
                     Person(5, "Name05", "Surname05", 55))
     ).doOnNext { person -> logger.info("Server Produces: $person") }
+
+    @GetMapping("/stream", produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    fun findPersonStream(): Flux<Person> = Flux.fromStream(
+            Stream.of(
+                    Person(6, "Name06", "Surname06", 66),
+                    Person(7, "Name07", "Surname07", 77),
+                    Person(8, "Name08", "Surname08", 88),
+                    Person(9, "Name09", "Surname09", 99),
+                    Person(10, "Name10", "Surname10", 100))
+    ).delaySequence(Duration.ofMillis(1000))
+            .doOnNext { person -> logger.info("Server Produces: $person") }
 }
