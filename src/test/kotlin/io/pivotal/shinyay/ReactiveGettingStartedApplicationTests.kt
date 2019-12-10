189526b45092ad37a5cb5aeb1fff15a9a3cb66c9
shinyay <shinya.com@gmail.com>
2019-12-10 19:44:16 +0900

    Add Flux test code

--- src/test/kotlin/io/pivotal/shinyay/ReactiveGettingStartedApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/ReactiveGettingStartedApplicationTests.kt
@@ -1,10 +1,12 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.entity.Person
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
 
 @SpringBootTest
 class ReactiveGettingStartedApplicationTests {
@@ -14,6 +16,8 @@ class ReactiveGettingStartedApplicationTests {
 
 	@Test
 	fun testFindPersonBackPressure() {
+		var person: Flux<Person> = webClient.get().uri("/backpressure").retrieve().bodyToFlux(Person::class.java)
+		person.map{Thread.sleep(90)}.subscribe { person -> logger.info("Client subscribes: $person") }
 	}
 
 }
