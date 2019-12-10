b3f46d6b80a258ce8ef2be582e7a8d2f64a42a89
shinyay <shinya.com@gmail.com>
2019-12-10 19:29:59 +0900

    Add TestClass abstract framework for BackPressure Reactive Stream

--- src/test/kotlin/io/pivotal/shinyay/ReactiveGettingStartedApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/ReactiveGettingStartedApplicationTests.kt
@@ -1,13 +1,19 @@
 package io.pivotal.shinyay
 
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootTest
 class ReactiveGettingStartedApplicationTests {
 
+	val logger: Logger = LoggerFactory.getLogger(this::class.java)
+	val webClient: WebClient = WebClient.builder().baseUrl("http://localhost:8080").build()
+
 	@Test
-	fun contextLoads() {
+	fun testFindPersonBackPressure() {
 	}
 
 }
