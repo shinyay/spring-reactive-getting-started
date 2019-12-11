9b6e02955571efe563be0a94d9894f590547b529
shinyay <shinya.com@gmail.com>
2019-12-11 17:50:08 +0900

    Add Employee entity

---
+++ src/main/kotlin/io/pivotal/shinyay/entity/Employee.kt
@@ -0,0 +1,4 @@
+package io.pivotal.shinyay.entity
+
+data class Employee(val id: String,
+                    val name: String)
--- src/main/kotlin/io/pivotal/shinyay/entity/Person.kt
+++
@@ -1,6 +0,0 @@
-package io.pivotal.shinyay.entity
-
-data class Person(val id: Int,
-                  val firstName: String,
-                  val lastName: String,
-                  val age: Int)
