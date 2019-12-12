de4d55f7645e5849c964e3de68bbcaecf24f9fd1
shinyay <shinya.com@gmail.com>
2019-12-13 07:48:27 +0900

    Add controller for calling repository to find employee by id

--- src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
@@ -1,12 +1,18 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.repository.EmployeeRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/employees")
-class EmployeeController {
+class EmployeeController(val employeeRepository: EmployeeRepository) {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @GetMapping("/{id}")
+    fun getEmployeeById(@PathVariable id: String) = employeeRepository.findEmployeeById(id)
 }
