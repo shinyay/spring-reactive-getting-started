613276a7f0743e01f331e35cd8f5b487e4acb909
shinyay <shinya.com@gmail.com>
2019-12-13 07:18:24 +0900

    Add Static Initilized data as Companion Object

--- src/main/kotlin/io/pivotal/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/io/pivotal/shinyay/repository/EmployeeRepository.kt
@@ -8,7 +8,21 @@ class EmployeeRepository {
 
     companion object {
         init {
-            val employeeData = mapOf<String, Employee>()
+            val employeeData = mapOf<String, Employee>(
+                    "1" to Employee("1", "Emp1"),
+                    "2" to Employee("2", "Emp2"),
+                    "3" to Employee("3", "Emp3"),
+                    "4" to Employee("4", "Emp4"),
+                    "5" to Employee("5", "Emp5")
+            )
+
+            val employeeAccessData = mapOf<String, String>(
+                    "1" to "Emp1 Access Data",
+                    "2" to "Emp2 Access Data",
+                    "3" to "Emp3 Access Data",
+                    "4" to "Emp4 Access Data",
+                    "5" to "Emp5 Access Data"
+                    )
         }
     }
 }
