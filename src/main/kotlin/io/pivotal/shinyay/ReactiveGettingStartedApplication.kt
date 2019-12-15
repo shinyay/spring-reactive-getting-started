6f0804290acb79e9308e1831e28c95cfb84a9142
shinyay <shinya.com@gmail.com>
2019-12-15 20:25:07 +0900

    Add webclient to register application

--- src/main/kotlin/io/pivotal/shinyay/ReactiveGettingStartedApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/ReactiveGettingStartedApplication.kt
@@ -1,5 +1,6 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.client.EmployeeWebClient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,6 @@ class ReactiveGettingStartedApplication
 
 fun main(args: Array<String>) {
 	runApplication<ReactiveGettingStartedApplication>(*args)
+	val employeeWebClient = EmployeeWebClient()
+	employeeWebClient.consume()
 }
