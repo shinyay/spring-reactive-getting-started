f513f700fac9fc257852d928c728ee867580ee99
shinyay <shinya.com@gmail.com>
2019-12-16 21:08:53 +0900

    Add webclient to register Flux function

--- src/main/kotlin/io/pivotal/shinyay/client/EmployeeWebClient.kt
+++ src/main/kotlin/io/pivotal/shinyay/client/EmployeeWebClient.kt
@@ -2,6 +2,7 @@ package io.pivotal.shinyay.client
 
 import io.pivotal.shinyay.entity.Employee
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 class EmployeeWebClient {
@@ -14,5 +15,11 @@ class EmployeeWebClient {
                 .retrieve()
                 .bodyToMono(Employee::class.java)
         employeeMono.subscribe(System.out::println)
+
+        val employeeFlux: Flux<Employee> = client.get()
+                .uri("/employees")
+                .retrieve()
+                .bodyToFlux(Employee::class.java)
+        employeeFlux.subscribe(System.out::println)
     }
 }
