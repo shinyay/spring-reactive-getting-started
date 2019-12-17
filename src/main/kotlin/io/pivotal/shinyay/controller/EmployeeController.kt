f79c368f4228327138453698e15f3ba81a09339d
shinyay <shinya.com@gmail.com>
2019-12-18 07:22:25 +0900

    Add update function for employee at Controller

--- src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/io/pivotal/shinyay/controller/EmployeeController.kt
@@ -1,12 +1,11 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.entity.Employee
 import io.pivotal.shinyay.repository.EmployeeRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/employees")
@@ -18,4 +17,7 @@ class EmployeeController(val employeeRepository: EmployeeRepository) {
 
     @GetMapping
     fun getAllEmployees() = employeeRepository.findAllEmployees()
+
+    @PostMapping("/update")
+    fun updateEmployee(@RequestBody employee: Employee): Mono<Employee> = employeeRepository.updateEmployee(employee)
 }
