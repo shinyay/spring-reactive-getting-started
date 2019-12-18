5a0aadb4a67db3749f099f942d34c18db6b04698
shinyay <shinya.com@gmail.com>
2019-12-19 07:25:51 +0900

    Add WebFluxSecurity enable annotation

---
+++ src/main/kotlin/io/pivotal/shinyay/security/EmployeeWebSecurityConfig.kt
@@ -0,0 +1,7 @@
+package io.pivotal.shinyay.security
+
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+
+@EnableWebFluxSecurity
+class EmployeeWebSecurityConfig {
+}
