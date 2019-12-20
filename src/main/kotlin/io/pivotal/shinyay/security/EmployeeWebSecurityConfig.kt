9eef04270835ccf85d14cc58064d07d863bdfff2
shinyay <shinya.com@gmail.com>
2019-12-20 09:19:42 +0900

    Add mock user for administration for accessing app

--- src/main/kotlin/io/pivotal/shinyay/security/EmployeeWebSecurityConfig.kt
+++ src/main/kotlin/io/pivotal/shinyay/security/EmployeeWebSecurityConfig.kt
@@ -1,7 +1,20 @@
 package io.pivotal.shinyay.security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @EnableWebFluxSecurity
 class EmployeeWebSecurityConfig {
+
+    @Bean
+    fun userDetailsService(): MapReactiveUserDetailsService = MapReactiveUserDetailsService(
+            User
+                    .withUsername("admin")
+                    .password(BCryptPasswordEncoder().encode("password"))
+                    .roles("ADMIN")
+                    .build()
+    )
 }
